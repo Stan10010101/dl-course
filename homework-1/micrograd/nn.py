d65f7c49f7c1e88e5881733542c402cc74259890
Stan10010101 <104889414+Stan10010101@users.noreply.github.com>
2022-12-28 10:33:58 +0300

    Update nn.py

--- homework-1/micrograd/nn.py
+++ homework-1/micrograd/nn.py
@@ -24,27 +24,43 @@ class Linear(Module):
     def __init__(self, in_features, out_features, bias: bool = True):
         """Initializing model"""
         # Create Linear Module
-
+        super(Linear, self).__init__()
+        stdv = 1./np.sqrt(in_features)
+        self.w = np.random.uniform(-stdv, stdv, size = (out_features, in_features)) 
+        self.b = np.random.uniform(-stdv, stdv, size = out_features)
+        
+        self.gradw = np.zeros_like(self.w)
+        self.gradb = np.zeros_like(self.b)
+        
     def forward(self, inp):
         """Y = W * x + b"""
-        return ...
+        self.output = np.dot(inp, self.w.T) + self.b
+        return self.output 
 
     def parameters(self):
         # return 1-d list of all parameters List[Value]
-        return ...
+        return [self.w, self.b]
 
 
 class ReLU(Module):
     """The most simple and popular activation function"""
-
+       def __init__(self):
+            super(ReLu, self).__init__()
+            
     def forward(self, inp):
         # Create ReLU Module
-        return ...
+        self.output = np.maximum(inp,0)
+        return self.output
 
 
 class CrossEntropyLoss(Module):
     """Cross-entropy loss for multi-class classification"""
-
+    def __init__(self):
+        q = super(ClassNLLCriterion, self)
+        super(ClassNLLCriterion, self).__init__()
+    
     def forward(self, inp, label):
         # Create CrossEntropy Loss Module
+        self.output = - np.mean(inp[np.arange(target.shape[0]), target])
+        return self.output
         return ...
