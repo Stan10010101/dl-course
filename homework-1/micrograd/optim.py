25ae70371babb4b15e77331870b933d8531eab19
Stan10010101 <104889414+Stan10010101@users.noreply.github.com>
2022-12-29 19:37:21 +0300

    Update optim.py

--- homework-1/micrograd/optim.py
+++ homework-1/micrograd/optim.py
@@ -1,11 +1,19 @@
 class SGD:
-    def __init__(self, parameters, lr):
+    def __init__(self, parameters, learning_rate):
         self.parameters = parameters
-        self.lr = lr
+        self.learning_rate = learning_rate
 
     def step(self):
         """Applying gradient descent to parameters"""
         # Implement SGD!
+        
+    
+        var_index = 0
+        for current_layer_params, current_layer_lrs in zip(parameters, learning_rate):
+            for current_lr, current_param in zip(current_layer_params, current_layer_lrs):
+            
+                current_lr -= config['learning_rate'] * current_param
+                var_index += 1 
 
     def zero_grad(self):
         """Resetting gradient for all parameters (set gradient to zero)"""
